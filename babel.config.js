82c805860c07c1f37e6ef6e46dbf9c8f76bd967d
Patrick Mwangi <frashid274@gmail.com>
2025-10-18 18:49:03 +0300

    chore: fix babel plugin order for worklets

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,19 @@
 module.exports = function (api) {
-  api.cache(true);
-  api.cache(true);
-  return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-    plugins: [
-      'react-native-worklets/plugin',
-      ["module-resolver", {
-        root: ["./"],
-        alias: {
-          "@": "./",
-          "tailwind.config": "./tailwind.config.js"
-        }
-      }]
-    ],
-  };
+    api.cache(true);
+    return {
+        presets: [
+            ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+            "nativewind/babel",
+        ],
+        plugins: [
+            ["module-resolver", {
+                root: ["./"],
+                alias: {
+                    "@": "./",
+                    "tailwind.config": "./tailwind.config.js"
+                }
+            }],
+            'react-native-worklets/plugin', // Moved to last position
+        ],
+    };
 };
